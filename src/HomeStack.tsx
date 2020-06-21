0f5009cb35471a2bc9d5ceed08aea45b1e0d6518
Iryna Trush <irynatrush@Irynas-MacBook-Air.local>
2020-06-21 20:48:11 +0300

    connected stack screen data with screen header

--- src/HomeStack.tsx
+++ src/HomeStack.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState, useEffect } from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import { Center } from './Center';
 import { Text, TouchableOpacity, FlatList, Button } from 'react-native';
@@ -45,7 +45,23 @@ function Product({route, navigation} : HomeStackNavProps<'Product'>) {
     );
 }
 
-function EditProduct({route} : HomeStackNavProps<'EditProduct'>) {
+function apiCall(x: any) {
+    return x
+}
+
+function EditProduct({route, navigation} : HomeStackNavProps<'EditProduct'>) {
+    const [formState, setFormState] = useState();
+    const submit = useRef(() => {});
+    submit.current = () => {
+        //api call with a new form state
+        apiCall(formState);
+        navigation.goBack();
+    }
+
+    useEffect(() => {
+        navigation.setParams({ submit })
+    }, [])
+
     return(
         <Center>
             <Text>editing {route.params.name}...</Text>
@@ -61,7 +77,16 @@ export const HomeStack: React.FC<HomeStackProps> = ({}) => {
                 name="EditProduct"
                 component={EditProduct}
                 options={({route}) => ({
-                    headerTitle: `Edit: ${route.params.name}`
+                    headerTitle: `Edit: ${route.params.name}`,
+                    headerRight: () => <TouchableOpacity 
+                    onPress={() => {
+                        if (route.params.submit) {
+                            route.params.submit?.current()
+                        }
+                    }}
+                    style={{paddingRight: 10}}>
+                        <Text style={{color: 'red'}}>Done</Text>
+                    </TouchableOpacity>
                 })}
                 />
                 <Stack.Screen 
